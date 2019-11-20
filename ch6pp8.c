f449c8b1d84b91f38242b5467cd153098ddfe396
Pawel <pawelstepniak22@gmail.com>
2019-11-20 18:05:03 +0100

    Not finish ch6pp8

---
+++ ch6pp8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int days, start; 
+
+  printf("Enter nummber of days in month: ");
+  scanf("%d", &days);
+  printf("Enter starting day of the week (1=Sun, 7=Sat): ");
+  scanf("%d", &start);
+
+  
+}
